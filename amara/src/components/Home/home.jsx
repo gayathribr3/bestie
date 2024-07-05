25f8565e4bd320858b3ba82c6b2fbaadff177e29
gayathribr3 <gayathribr319@gmail.com>
2024-07-06 01:30:55 +0530

    first commit

---
+++ amara/src/components/Home/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      <h2>jhjghg</h2>
+    </div>
+  )
+}
+
+export default home
---
+++ amara/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <img src="icons8-love-64.png" alt="" />
+      
+    </div>
+  )
+}
+
+export default Navbar;
